80df40858de6bcca3a0f68788e21115a69714d65
luccasirineu <luccas19100@outlook.com>
2024-05-13 16:14:00 -0300

    criando o projeto e adicionando o lombok

---
+++ demo/src/main/java/br/com/lucccasdev/api/ProjectTddApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucccasdev.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTddApplication.class, args);
+	}
+
+}
---
+++ demo/src/main/java/br/com/lucccasdev/api/domain/User.java
@@ -0,0 +1,23 @@
+package br.com.lucccasdev.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Data // caso a classe tenha muitos atributos essa anotation pode deixar a api menos perfomatica
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
