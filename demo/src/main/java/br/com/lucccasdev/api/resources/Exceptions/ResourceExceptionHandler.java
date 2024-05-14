3bcdab993a8d5bb7022f8c1fe08c2033fd3d1a29
luccasirineu <luccas19100@outlook.com>
2024-05-14 15:20:03 -0300

    criação da exception email ja cadastrado

--- demo/src/main/java/br/com/lucccasdev/api/resources/Exceptions/ResourceExceptionHandler.java
+++ demo/src/main/java/br/com/lucccasdev/api/resources/Exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.lucccasdev.api.resources.Exceptions;
 
+import br.com.lucccasdev.api.services.exceptions.DataIntegratyViolationException;
 import br.com.lucccasdev.api.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.coyote.Response;
@@ -17,5 +18,11 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request){
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegratyViolationException.class)
+    public ResponseEntity<StandardError> emailAlreadyExist(DataIntegratyViolationException ex, HttpServletRequest request){
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- demo/src/main/java/br/com/lucccasdev/api/services/impl/UserServiceImpl.java
+++ demo/src/main/java/br/com/lucccasdev/api/services/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.lucccasdev.api.domain.User;
 import br.com.lucccasdev.api.domain.dto.UserDTO;
 import br.com.lucccasdev.api.repositories.UserRepository;
 import br.com.lucccasdev.api.services.UserService;
+import br.com.lucccasdev.api.services.exceptions.DataIntegratyViolationException;
 import br.com.lucccasdev.api.services.exceptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(UserDTO obj) {
+        findByEmail(obj);
         User user = mapper.map(obj, User.class);
         return repository.save(user);
+    }
+
+    public void findByEmail(UserDTO obj){
+        Optional<User> user = repository.findByEmail(obj.getEmail());
+        if(user.isPresent()){
+            throw new DataIntegratyViolationException("Email already exist");
+        }
     }
 }
