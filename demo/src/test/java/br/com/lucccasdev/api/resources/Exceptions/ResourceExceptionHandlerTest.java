9ad33076df53df78b456f47647a9925d4d1a0f67
luccasirineu <luccas19100@outlook.com>
2024-05-15 18:22:04 -0300

    Teste UserResource dataIntegratyViolation

--- demo/src/test/java/br/com/lucccasdev/api/resources/Exceptions/ResourceExceptionHandlerTest.java
+++ demo/src/test/java/br/com/lucccasdev/api/resources/Exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.lucccasdev.api.resources.Exceptions;
 
+import br.com.lucccasdev.api.services.exceptions.DataIntegratyViolationException;
 import br.com.lucccasdev.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,6 +36,16 @@ class ResourceExceptionHandlerTest {
     }
 
     @Test
-    void emailAlreadyExist() {
+    void dataIntegratyViolation() {
+        ResponseEntity<StandardError> response = exceptionHandler.dataIntegratyViolation(new DataIntegratyViolationException("Email ja cadastrado"), new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals("Email ja cadastrado", response.getBody().getError());
+
     }
 }
